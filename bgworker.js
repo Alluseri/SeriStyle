307ae80d266b5e677ae02c689bcc315629bd3b68
Alluseri <42897348+Alluseri@users.noreply.github.com>
2022-11-29 22:23:06 +0500

    Fix Google Chrome incompatibility

--- bgworker.js
+++ bgworker.js
@@ -1,10 +1,12 @@
 /* jshint esversion: 11 */
 
+const SeriStyle_GX = false;
+
 chrome.runtime.onMessage.addListener((Message, Sender) => {
 	switch (Message.Operation) {
 		case "SeriStyle_LoadScript":
 			chrome.scripting.executeScript({
-				target: { tabId: Sender.tab.id },
+				target: { tabId: Sender.tab.id, documentIds: (!SeriStyle_GX && Sender.documentId) ? [Sender.documentId] : undefined },
 				files: [Message.Args[0]]
 			});
 			return;
